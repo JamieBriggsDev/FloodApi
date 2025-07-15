101b0b18f38d53257981305cf158569f3703a615
Jamie Briggs <personal@jbriggs.dev>
2025-07-15 23:51:36 +0100

    Refactored setup to dynamically allocate LiquidCrystalAdapter and updated Printer initialization.

--- api/src/main.cpp
+++ api/src/main.cpp
@@ -10,13 +10,15 @@
 
 
 FloodRoutes* flood_routes;
+LiquidCrystalAdapter* lcd;
 Printer* printer;
 
 void setup()
 {
     Serial.begin(115200);
-    LiquidCrystalAdapter lcd(ESP_RS_PIN, ESP_ENABLE_PIN, ESP_D0_PIN, ESP_D1_PIN, ESP_D2_PIN, ESP_D3_PIN);
-    printer = new Printer(lcd);
+    lcd =
+        new LiquidCrystalAdapter(ESP_RS_PIN, ESP_ENABLE_PIN, ESP_D0_PIN, ESP_D1_PIN, ESP_D2_PIN, ESP_D3_PIN);
+    printer = new Printer(*lcd);
     printer->println("Starting", "Flood App!", STICKY);
     flood_routes = new FloodRoutes(printer);
 }
