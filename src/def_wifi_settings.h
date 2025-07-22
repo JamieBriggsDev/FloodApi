c48693c9f49bb15542dac2a8aa837438c4089d86
Jamie Briggs <personal@jbriggs.dev>
2025-07-22 21:04:03 +0100

    Added def_wifi_settings.h

---
+++ src/def_wifi_settings.h
@@ -0,0 +1,19 @@
+//
+// Created by Jamie Briggs on 10/07/2025.
+//
+
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#include <IPAddress.h>
+
+extern const char* WIFI_SSID;
+extern const char* WIFI_PASSWORD;
+
+extern const IPAddress DB_HOSTNAME;
+extern int DB_PORT;
+extern char DB_NAME[];
+extern char DB_USERNAME[];
+extern char DB_PASSWORD[];
+
+#endif //CREDENTIALS_H
