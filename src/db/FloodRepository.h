a79f71eaba46091a0216d675e5a858c81a098ae1
Jamie Briggs <personal@jbriggs.dev>
2025-07-24 10:03:46 +0100

    Reverted FloodRepository.cpp to use SQLite3

--- src/db/FloodRepository.h
+++ src/db/FloodRepository.h
@@ -5,10 +5,13 @@
 #ifndef FLOODREPOSITORY_H
 #define FLOODREPOSITORY_H
 
+#include <cstdint>
+#include <mutex>
 #include <vector>
 
 
 #include "IFloodRepository.h"
+#include "sqlite3.h"
 
 // Forward declarations, avoids adding the header in this file
 struct RiverReading;
@@ -16,11 +19,18 @@ struct RainfallReading;
 
 class FloodRepository : public IFloodRepository
 {
-
+  sqlite3* m_floodDb;
+  mutable std::mutex m_mutex;  // mutable allows usage in const methods
 
 
   public:
-  void init() override;
+  FloodRepository() : m_floodDb(nullptr) {};
+  ~FloodRepository() override
+  {
+    sqlite3_close(m_floodDb);
+    m_floodDb = nullptr;
+  };
+  void init();
   std::vector<RiverReading> getRiverReadings(const char* startDate, uint16_t page,
                                              uint8_t pageSize) const override;
   std::vector<RainfallReading> getStationRainfallReadings(const char* stationName, const char* startDate,
